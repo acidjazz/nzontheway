744e0e0bedb248f73e2aedf6fb2053d9942f07b7
kevin olson <acidjazz@gmail.com>
2013-04-29 20:35:25 +0000

    added #nzontheway

--- crn/cache.php
+++ crn/cache.php
@@ -4,5 +4,5 @@ $path = dirname(realpath($argv[0]));
 require_once $path.'/../cfg/config.php';
 
 $ic = new instacache(24*60*4);
-$ic->cache(['maricondia']);
+$ic->cache(['maricondia','nzontheway']);
 
